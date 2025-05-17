b5ba0ddeb03787746b8b3d04a068d04187a4a8b5
oscarrojas <oscar@okane.capital>
2025-05-17 14:18:28 -0400

    initialize Vue 3 project with Vite, add basic structure and configuration files

---
+++ src/components/home/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img src="@/assets/img/pikachu.svg" alt="pikachu" />
+    <h1 class="home-title">Welcome to Pokédex</h1>
+    <p>
+      The digital encyclopedia created by Professor Oak is an invaluable tool to Trainers in the
+      Pokémon world.
+    </p>
+    <div class="button">Get started</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  height: 100vh;
+  gap: 2rem;
+  text-align: center;
+  padding: 2rem;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Index from "@/components/home/Index.vue";
+</script>
+
+<template>
+  <div>
+    <Index />
+  </div>
+</template>
